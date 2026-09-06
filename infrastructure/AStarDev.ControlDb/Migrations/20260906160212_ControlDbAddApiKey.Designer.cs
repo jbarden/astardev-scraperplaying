fe090277795fa80827817137ae044ccc33540cce
Jason Barden <jason.barden@outlook.com>
2026-09-06 17:09:34 +0100

    Add ApiKey column to UserConfigurations and update model snapshot

--- infrastructure/AStarDev.ControlDb/Migrations/20260906160212_ControlDbAddApiKey.Designer.cs
+++ infrastructure/AStarDev.ControlDb/Migrations/20260906160212_ControlDbAddApiKey.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AStarDev.ControlDb;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AStarDev.ControlDb.Migrations
 {
     [DbContext(typeof(ControlDbContext))]
-    partial class ControlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260906160212_ControlDbAddApiKey")]
+    partial class ControlDbAddApiKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.11");
