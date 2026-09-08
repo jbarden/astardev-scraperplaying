483cc3b02ba1c473213aa819630765740fefa53b
Jason Barden <jason.barden@outlook.com>
2026-09-08 08:58:08 +0100

    Refactor ConfigurationFilePicker to streamline file selection logic and improve readability

--- AStarDev.ScraperPlaying/Home/ConfigurationFilePicker.cs
+++ AStarDev.ScraperPlaying/Home/ConfigurationFilePicker.cs
@@ -8,19 +8,13 @@ public sealed class ConfigurationFilePicker : IConfigurationFilePicker
 {
     public async Task<Option<string>> PickAsync(Window owner)
     {
-        var options = new FilePickerOpenOptions
+        var files = await owner.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
         {
             AllowMultiple = false,
             Title = "Import scrape configuration",
             FileTypeFilter = [new FilePickerFileType("JSON configuration") { Patterns = ["*.json"] }]
-        };
-        var files = await owner.StorageProvider.OpenFilePickerAsync(options);
+        });
 
-        if (files.Count == 0)
-        {
-            return Option<string>.None.Instance;
-        }
-
-        return new Option<string>.Some(files[0].Path.LocalPath);
+        return files.Count == 0 ? null! : files[0].Path.LocalPath;
     }
 }
